9430a702d3f1e9dea43ccb744b85fa2a9462a566
Jer D Mul <homer2320776@users.noreply.github.com>
2021-10-08 10:04:23 -0400

    Update Amazon_Shortner_Bookmarklet.js

--- bookmarklets/Amazon_Shortner_Bookmarklet.js
+++ bookmarklets/Amazon_Shortner_Bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(x){var asin = x.getElementById('ASIN');var url='https://amzn.com/'+asin.value;navigator.clipboard.writeText(url);alert('Copied to clipboard\n'+url);}(document))
+javascript:(function(x){var asin = x.getElementById('ASIN');var url='https://amzn.com/dp/'+asin.value;navigator.clipboard.writeText(url);alert('Copied to clipboard\n'+url);}(document))
